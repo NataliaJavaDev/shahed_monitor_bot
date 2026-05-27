b75311882ae89119f8938df7639e4417da77c210
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-05-27 16:33:11 +0300

    first commit

---
+++ src/main/java/com/tgbot/shahed_monitor_bot/ShahedMonitorBotApplication.java
@@ -0,0 +1,13 @@
+package com.tgbot.shahed_monitor_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShahedMonitorBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShahedMonitorBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgbot/shahed_monitor_bot/ShahedMonitorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgbot.shahed_monitor_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShahedMonitorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
