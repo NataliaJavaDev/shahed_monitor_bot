0a680cfd28d4246cfc5dd872be9e34c1d73ed971
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-06-04 21:51:17 +0300

    Detect alert type from API response

--- src/main/java/com/tgbot/shahedmonitorbot/alertapi/service/AirAlertApiService.java
+++ src/main/java/com/tgbot/shahedmonitorbot/alertapi/service/AirAlertApiService.java
@@ -2,6 +2,8 @@ package com.tgbot.shahedmonitorbot.alertapi.service;
 
 import com.tgbot.shahedmonitorbot.alertapi.client.AirAlertApiClient;
 import com.tgbot.shahedmonitorbot.alertapi.dto.RegionAlertDto;
+import com.tgbot.shahedmonitorbot.config.AppProperties;
+import com.tgbot.shahedmonitorbot.manualalert.ManualAlertType;
 
 import java.util.Arrays;
 
@@ -11,20 +13,45 @@ import org.springframework.stereotype.Service;
 public class AirAlertApiService {
 
     private final AirAlertApiClient client;
+    private final AppProperties properties;
 
-    public AirAlertApiService(AirAlertApiClient client) {
+    public AirAlertApiService(
+        AirAlertApiClient client,
+        AppProperties properties
+    ) {
         this.client = client;
+        this.properties = properties;
     }
 
     public void checkAlerts() {
 
         RegionAlertDto[] alerts = client.fetchAlerts();
 
-        boolean districtAlert = Arrays.stream(alerts)
-            .anyMatch(alert -> "73".equals(alert.regionId()));
+        ManualAlertType type = detectAlertType(alerts);
 
-        System.out.println(
-                "Bila Tserkva district alert: " + districtAlert
-        );
+        System.out.println("API alert type: " + type);
     }
+
+    private ManualAlertType detectAlertType(RegionAlertDto[] alerts) {
+
+    boolean highRisk = Arrays.stream(alerts)
+            .anyMatch(alert -> properties.alertApi()
+                    .dangerRegionIds()
+                    .contains(alert.regionId()));
+
+    if (highRisk) {
+        return ManualAlertType.HIGH_RISK;
+    }
+
+    boolean districtAlert = Arrays.stream(alerts)
+            .anyMatch(alert -> properties.alertApi()
+                    .alarmRegionId()
+                    .equals(alert.regionId()));
+
+    if (districtAlert) {
+        return ManualAlertType.ALERT;
+    }
+
+    return ManualAlertType.ALL_CLEAR;
+}
 }
