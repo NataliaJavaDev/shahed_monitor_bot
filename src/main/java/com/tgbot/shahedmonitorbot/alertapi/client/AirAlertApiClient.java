a654660a61964a5afb416d6f61bff59abf9f4de4
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-06-04 20:24:01 +0300

    Connect to Ukraine Alarm API

--- src/main/java/com/tgbot/shahedmonitorbot/alertapi/client/AirAlertApiClient.java
+++ src/main/java/com/tgbot/shahedmonitorbot/alertapi/client/AirAlertApiClient.java
@@ -1,11 +1,31 @@
 package com.tgbot.shahedmonitorbot.alertapi.client;
 
 import org.springframework.stereotype.Component;
+import com.tgbot.shahedmonitorbot.config.AppProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 @Component
 public class AirAlertApiClient {
 
+    private final RestClient restClient;
+    private final AppProperties properties;
+
+    public AirAlertApiClient(
+            RestClient.Builder restClientBuilder,
+            AppProperties properties
+    ) {
+        this.properties = properties;
+        this.restClient = restClientBuilder
+                .baseUrl(properties.alertApi().baseUrl())
+                .defaultHeader("Authorization", properties.alertApi().apiKey())
+                .build();
+    }
+
     public String fetchAlerts() {
-        return "API client works";
+        return restClient.get()
+                .uri("/alerts")
+                .retrieve()
+                .body(String.class);
     }
 }
