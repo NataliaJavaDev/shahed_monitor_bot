842a25452e63415aeab451891a25094dc7ac2155
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-06-05 15:54:07 +0300

    Fix logger

--- src/main/java/com/tgbot/shahedmonitorbot/alertapi/scheduler/AirAlertPollingScheduler.java
+++ src/main/java/com/tgbot/shahedmonitorbot/alertapi/scheduler/AirAlertPollingScheduler.java
@@ -1,6 +1,9 @@
 package com.tgbot.shahedmonitorbot.alertapi.scheduler;
 
 import com.tgbot.shahedmonitorbot.alertapi.service.AirAlertApiService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +11,8 @@ import org.springframework.stereotype.Component;
 public class AirAlertPollingScheduler {
 
     private final AirAlertApiService service;
+    private static final Logger log =
+            LoggerFactory.getLogger(AirAlertPollingScheduler.class);
 
     public AirAlertPollingScheduler(AirAlertApiService service) {
         this.service = service;
