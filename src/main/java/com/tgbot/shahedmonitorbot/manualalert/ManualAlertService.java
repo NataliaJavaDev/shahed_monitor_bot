7b1097b560dab08d094285832304c97c09a631eb
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-06-10 21:44:09 +0300

    Add API control toggle for monitoring

--- src/main/java/com/tgbot/shahedmonitorbot/manualalert/ManualAlertService.java
+++ src/main/java/com/tgbot/shahedmonitorbot/manualalert/ManualAlertService.java
@@ -1,5 +1,6 @@
 package com.tgbot.shahedmonitorbot.manualalert;
 
+import com.tgbot.shahedmonitorbot.monitoring.MonitoringStateService;
 import com.tgbot.shahedmonitorbot.alert.AlertDeliveryService;
 import org.springframework.stereotype.Service;
 
@@ -8,16 +9,31 @@ public class ManualAlertService {
 
     private final AlertDeliveryService alertDeliveryService;
     private final ManualAlertMessageFormatter formatter;
+    private final MonitoringStateService monitoringStateService;
+
 
     public ManualAlertService(
             AlertDeliveryService alertDeliveryService,
-            ManualAlertMessageFormatter formatter
+            ManualAlertMessageFormatter formatter,
+            MonitoringStateService monitoringStateService
     ) {
         this.alertDeliveryService = alertDeliveryService;
         this.formatter = formatter;
+        this.monitoringStateService = monitoringStateService;
     }
 
-    public void sendAlert(ManualAlertType type) {
-        alertDeliveryService.send(formatter.format(type));
+    private void updateMonitoringFromManual(ManualAlertType type) {
+        
+        if (monitoringStateService.isApiControlEnabled()) {
+            return;
+        }
+
+        if (type == ManualAlertType.ALERT) {
+            monitoringStateService.enableMonitoring();
+        }
+
+        if (type == ManualAlertType.ALL_CLEAR) {
+            monitoringStateService.disableMonitoring();
+        }
     }
 }
