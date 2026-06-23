fcb874700e19e6761a74cda83f8c436aef8d53e9
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-06-23 21:24:03 +0300

    .

--- src/main/java/com/tgbot/shahedmonitorbot/tdlib/TdJsonLibrary.java
+++ src/main/java/com/tgbot/shahedmonitorbot/tdlib/TdJsonLibrary.java
@@ -1,14 +1,14 @@
 package com.tgbot.shahedmonitorbot.tdlib;
 
 import com.sun.jna.Library;
-import com.sun.jna.Native;
+//import com.sun.jna.Native;
 
 public interface TdJsonLibrary extends Library {
 
-    TdJsonLibrary INSTANCE = Native.load(
-        "/home/natta/tdlib-build/td/build/libtdjson.so",
-        TdJsonLibrary.class
-    );
+    // TdJsonLibrary INSTANCE = Native.load(
+    //     "/home/natta/tdlib-build/td/build/libtdjson.so",
+    //     TdJsonLibrary.class
+    // );
 
     int td_create_client_id();
 
