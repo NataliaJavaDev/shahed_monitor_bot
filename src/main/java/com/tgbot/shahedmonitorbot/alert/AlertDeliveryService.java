5c3972fa12e2af786f4340200ea0e4d91287e577
NataliaJavaDev <natalia.java.dev@gmail.com>
2026-08-21 18:51:42 +0300

    Send API alerts to debug group

--- src/main/java/com/tgbot/shahedmonitorbot/alert/AlertDeliveryService.java
+++ src/main/java/com/tgbot/shahedmonitorbot/alert/AlertDeliveryService.java
@@ -1,5 +1,6 @@
 package com.tgbot.shahedmonitorbot.alert;
 
+import com.tgbot.shahedmonitorbot.config.AppProperties;
 import com.tgbot.shahedmonitorbot.sender.TelegramSenderService;
 import org.springframework.stereotype.Service;
 
@@ -7,12 +8,15 @@ import org.springframework.stereotype.Service;
 public class AlertDeliveryService {
 
     private final TelegramSenderService senderService;
+    private final AppProperties properties;
 
-    public AlertDeliveryService(TelegramSenderService senderService) {
+    public AlertDeliveryService(TelegramSenderService senderService, AppProperties properties) {
         this.senderService = senderService;
+        this.properties = properties;
     }
 
     public void send(String message) {
-        senderService.send(message);
+        senderService.sendToChat(properties.telegram().targetChannelId(), message);
+        senderService.sendToChat(properties.telegram().debugChannelId(), message);
     }
 }
